b79e54c45a67827820b913587c81c09e5102d48d
Cory Mathis <coryrmathis@gmail.com>
2017-08-09 23:42:57 -0700

    slick working okay.

--- app/assets/javascripts/landing_page.js
+++ app/assets/javascripts/landing_page.js
@@ -1,20 +1,19 @@
 $(document).ready(function(){
+
   $(".fa-info").on("click", function(e){
     var aboutContainer = $("#about-container")
     e.preventDefault()
     aboutContainer.show("fold")
-    $("#projects-list").slide()
 
   })
 
   $(".fa-code").on("click", function(e){
     var aboutContainer = $("#projects-container")
     var projectsList = $("#projects-list")
-    aboutContainer.show("fold", {
-      complete: function(){
-        $("#projects-list").slick()
-      }
-    })
+    projectsList.slick({
+      init: aboutContainer.show("fold" )
+    }) 
+    
 
   })
 })
--- app/assets/javascripts/projects_container.js
+++ app/assets/javascripts/projects_container.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
-
+  var projectsList = $("#projects-list")
   var backBtn = $("#projects-container .back-btn")
 
-  backBtn.on("click", function(){
-    $(backBtn).closest("#projects-container").hide("fold")
+  $(document).on("click", "#projects-container .back-btn", function(){
+    
+    $(this).closest("#projects-container").hide("fold", {
+      complete: function(){
+        projectsList.slick("unslick")
+      }
+    })
+    
   })
 })
 
